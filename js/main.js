cddc874c536eb8def2454df0e0f847242a502827
R-H-Perez <R-H-Perez@users.noreply.github.com>
2021-10-19 23:31:47 +0000

    deploy: 71839560d9fea0ab8b67dda8edc1e623360d0a7d

--- js/main.js
+++ js/main.js
@@ -1,1 +1,59 @@
+/*
+Hey Jack. Keep your hands out of my stack.
+*/
 
+// String
+let classname = "Pink Floyd Band Members";
+
+// Number
+let classSize = 5;
+
+// Boolean
+let isRetired = true;
+
+// Array
+let Members = [
+  "Syd Barrett",
+  "Roger Waters",
+  "David Gilmour",
+  "Nick Mason",
+  "Richard Wright",
+];
+
+// Object
+let band = {
+  classname: "Pink Floyd Band Members",
+  classSize: 5,
+  isRetired: true,
+  Members: [
+    "Syd Barrett",
+    "Roger Waters",
+    "David Gilmour",
+    "Nick Mason",
+    "Richard Wright",
+  ],
+};
+
+// Conditionals
+let x = 1;
+let y = 2;
+
+if (x === y) {
+  console.log("It is equal!");
+} else if (x > y) {
+  console.log("It is greater than.");
+} else {
+  console.log("No cases matched.");
+}
+
+// Functions
+
+function singLyric(classname) {
+  console.log(`I have a mouse whose getting rather old. ${classname}.`);
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+let product = multiply(2, 3);
